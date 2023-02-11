fcf3e70ae17f0d15ccf67b4c97f9b803a53dfbe5
rishabh mathur <rishabh.mathur@rishabh.mathur-MacBookPro>
2023-02-11 19:15:19 +0530

    H1010-connnector added

--- crates/api_models/src/enums.rs
+++ crates/api_models/src/enums.rs
@@ -519,6 +519,7 @@ pub enum Connector {
     Cybersource,
     #[default]
     Dummy,
+	Forte,
     Fiserv,
     Globalpay,
     Klarna,
@@ -566,6 +567,7 @@ pub enum RoutableConnectors {
     Stripe,
     Worldline,
     Worldpay,
+    Forte,
 }
 
 /// Wallets which support obtaining session object
